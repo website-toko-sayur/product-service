139cae0eaa680752d588fbc525dbe64eb2c2068b
xylo <m.aditya3232@gmail.com>
2026-05-25 09:41:20 +0700

    fix upload image handler for admin

--- internal/adapter/handler/upload_image_handler.go
+++ internal/adapter/handler/upload_image_handler.go
@@ -1,9 +1,15 @@
 package handler
 
 import (
+	"product-service/config"
+	"product-service/internal/adapter"
+	"product-service/internal/adapter/handler/response"
 	"product-service/internal/adapter/storage"
+	"product-service/internal/core/service"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog/log"
 )
 
 type uploadImage struct {
@@ -12,4 +18,62 @@ type uploadImage struct {
 
 type UploadImageInterface interface {
 	UploadImage(c fiber.Ctx) error
+}
+
+func NewUploadImage(
+	app *fiber.App,
+	cfg *config.Config,
+	storageHandler storage.MinioStorageInterface,
+	jwtService service.JwtServiceInterface,
+	redis *redis.Client,
+) UploadImageInterface {
+	res := &uploadImage{
+		storageHandler: storageHandler,
+	}
+
+	mid := adapter.NewMiddlewareAdapter(cfg, jwtService, redis)
+	app.Post("/admin/image-upload", res.UploadImage, mid.CheckToken())
+
+	return res
+}
+
+func (u *uploadImage) UploadImage(c fiber.Ctx) error {
+	fileHeader, err := c.FormFile("image")
+	if err != nil {
+		log.Error().
+			Err(err).
+			Str("source", "internal.adapter.uploadImage.UploadImage").
+			Msg("failed get uploaded file")
+
+		return fiber.NewError(
+			fiber.StatusUnprocessableEntity,
+			err.Error(),
+		)
+	}
+
+	ctx := c.Context()
+
+	url, err := u.storageHandler.ProcessAndUploadImage(
+		ctx,
+		fileHeader,
+	)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Str("filename", fileHeader.Filename).
+			Str("source", "internal.adapter.uploadImage.UploadImage").
+			Msg("failed upload image")
+
+		return fiber.NewError(
+			fiber.StatusInternalServerError,
+			err.Error(),
+		)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(response.DefaultResponse{
+		Message: "success",
+		Data: map[string]string{
+			"image_url": url,
+		},
+	})
 }
